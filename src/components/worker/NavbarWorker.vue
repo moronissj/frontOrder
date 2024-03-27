5ce4002e4f18085fd2d47ebe2e1c41a83cf80f36
moronissj <104938057+moronissj@users.noreply.github.com>
2024-03-27 16:53:32 -0600

    Add files via upload

--- src/components/worker/NavbarWorker.vue
+++ src/components/worker/NavbarWorker.vue
@@ -1,29 +1,44 @@
 <template>
-    <div>
-      <b-navbar toggleable="lg" variant="dark" class="bg-dark">
-        <b-navbar-brand href="#"><img src="../../assets/logo.png" class="logo" alt="logo"></b-navbar-brand>
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-        <b-collapse id="nav-collapse" is-nav>
-          <b-navbar-nav class="my-1">
-  
-            <b-nav-item href="#" @click="goToServices" ><p class="itemsM">Solicitud de servicios</p ></b-nav-item>
+  <div >
+    <b-navbar toggleable="lg" variant="dark" class="bg-dark">
+      <b-navbar-brand href="#"><img src="../../assets/logo.png" class="logo" alt="logo"></b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="my-1">
 
-  
-            <b-nav-item-dropdown text="User" right class="ml-auto itemsM"> <!--HAZ QUE ESTE EL TEXT SEA BLNACO-->
-              <b-dropdown-item href="#" @click="goToLogin">Cerrar sesión</b-dropdown-item>
-            </b-nav-item-dropdown>
-          </b-navbar-nav>
-        </b-collapse>
-      </b-navbar>
-    </div>
-  </template>
+          <b-nav-item href="#" @click="goToSolicitudes" ><p class="itemsM">Solicitudes</p ></b-nav-item>
+           
+        </b-navbar-nav>
+
+        <b-nav-item-dropdown right class="ml-auto itemsM2">
+      <!-- Utiliza el slot "button-content" para personalizar el contenido del dropdown -->
+      <template #button-content style="height: 7.5vh; width: 7.5vh; ">
+        <img src="../../assets/buffe.PNG" class="user-avatar" alt="User">
+      </template>
+      <b-dropdown-item @click="goToAccount">Cuenta</b-dropdown-item>
+      <b-dropdown-item @click="goToLogin">Cerrar sesión</b-dropdown-item>
+    </b-nav-item-dropdown>
+      </b-collapse>
+      
+    </b-navbar>
+  </div>
+</template>
+
   
   <script>
   export default {
     methods: {
 
-      goToServices() {
+      goToSolicitudes() {
         this.$router.push('/solicitudes-trabajador');
+      },
+
+      goToAccount() {
+        this.$router.push('/perfil-trabajador');
+      },
+
+      goToLogin() {
+        this.$router.push('/login');
       },
 
   
@@ -32,33 +47,57 @@
   </script>
   
   <style scoped>
-  .logo {
-    height: 7.5vh;
-    width: 7.5vh;
+
+div{
+  width:100%
+}
+.logo {
+  height: 7.5vh;
+  width: 7.5vh;
+}
+.user-avatar {
+  height: 7.5vh; /* Ajusta esto según el tamaño que desees */
+  width: 7.5vh;
+  object-fit: cover;
+  border-radius: 50%; /* Esto creará una imagen redonda si es cuadrada */
+}
+
+/* Ensure all text in the navbar is white */
+.navbar-dark .b-navbar-nav .nav-link,
+.navbar-dark .b-navbar-brand,
+.navbar-dark .b-nav-item-dropdown > a,
+.navbar-dark .b-nav-item-dropdown .dropdown-toggle,
+.navbar-dark .b-nav-item-dropdown .dropdown-item {
+  color: white !important;
+}
+
+/* You may not need this if the above works, but just in case */
+.itemsM {
+  color: white !important;
+  font-family: 'Montserrat', sans-serif;
+  font-weight: bold;
+}
+
+.itemsM2 {
+  margin-left: 66%;
+  margin-bottom: 2vh;
+}
+
+@media (max-width: 800px)  {
+  .itemsM2{
+    margin-left: 0
   }
-  
-  /* Ensure all text in the navbar is white */
-  .navbar-dark .b-navbar-nav .nav-link,
-  .navbar-dark .b-navbar-brand,
-  .navbar-dark .b-nav-item-dropdown > a,
-  .navbar-dark .b-nav-item-dropdown .dropdown-toggle,
-  .navbar-dark .b-nav-item-dropdown .dropdown-item {
-    color: white !important;
-  }
-  
-  /* You may not need this if the above works, but just in case */
-  .itemsM {
-    color: white !important;
-    font-family: 'Montserrat', sans-serif;
-    font-weight: bold;
-  }
-  
-  /* Other existing styles */
-  /* ... */
-  .bg-dark {
-    background-color: #AE0505 !important;
-  }
-  
-  </style>
-  
-  
+}
+
+
+
+/* Other existing styles */
+/* ... */
+.bg-dark {
+  background-color: #AE0505 !important;
+
+}
+
+
+
+</style>
