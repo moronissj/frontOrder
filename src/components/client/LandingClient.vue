b85393591d22edbeacae2bdb530d03fee0ca887f
moronissj <104938057+moronissj@users.noreply.github.com>
2024-03-28 15:18:43 -0600

    Add files via upload

--- src/components/client/LandingClient.vue
+++ src/components/client/LandingClient.vue
@@ -1,212 +1,249 @@
 <script setup>
-import NavbarClient from './NavbarClient.vue';
-import Footer from '../../components/Footer.vue';
-import Carousel from '../../components/Carousel.vue';
-import RatingTaquiza from '../RatingTaquiza.vue';
-import FancyBoxLanding from '../FancyBoxLanding.vue';
-import FancyBoxTaquizas from '../FancyBoxTaquizas.vue';
+import NavbarClient from "./NavbarClient.vue";
+import Footer from "../Footer.vue";
+import Carousel from "../Carousel.vue";
+import RatingTaquiza from "../RatingTaquiza.vue";
 </script>
 
 <template>
-    <div>
-      <div class="app">
-        <NavbarClient/>
-      </div>
+  <div>
+    <NavbarClient />
   
-    <div class="banner-container">
-      <img src="../../assets/Banner.png" alt="" class="banner-image">
 
-    </div>
+<!-- Spinner de carga -->
+<div v-if="isLoading" class="spinner-fullscreen">
+  <div
+    class="spinner-border text-primary"
+    role="status"
+    style="width: 20rem; height: 20rem"
+  ></div>
+</div>
 
-
-    <Carousel/>
-    
-    <div class="title">
-        <h1>Servicios populares</h1>
-    </div>
-    <div class="servicios">     
-        <div class="card1" >
-            <h3>Transporte</h3>
-            <img src="../../assets/transporteIcon.png" alt="" class="iconService">
-            <div>
-                <b-button class="btn" @click="goToTransporte">Ver más</b-button>
-            </div>
-        </div>
-        <div class="card2">
-            <h3>Comida</h3>
-            <img src="../../assets/comidaIcon.png" alt="" class="iconService">
-            <div>
-                <b-button class="btn" @click="goToComida">Ver más</b-button>
-            </div>
-        </div>
-        <div class="card1">
-            <h3>Utileria</h3>
-            <img src="../../assets/utileriaIcon.png" alt="" class="iconService">
-            <div>
-                <b-button class="btn" @click="goToUtileria">Ver más</b-button>
-            </div>
-            
-        </div>
-
-    </div>
-
-    <div class="quienes-somos-container">
-    <div class="header-section">
-      <h1>¿Quiénes somos?</h1>
-    </div>
-    <div class="content-section">
-      <p>
-        Somos un equipo apasionado por la gastronomía y el buen servicio, dedicado a
-        ofrecer experiencias culinarias únicas a través de nuestros buffets. Con una
-        amplia variedad de opciones que se adaptan a cualquier tipo de evento, nuestro
-        compromiso es garantizar la satisfacción y superar las expectativas de nuestros
-        clientes.
-      </p>
-      <p>
-        Fundada en [Año], nuestra misión ha sido siempre llevar a cada evento un toque
-        especial, combinando sabores tradicionales y contemporáneos para crear menús
-        memorables. Valoramos la calidad, la innovación y la excelencia en el servicio,
-        y estamos constantemente explorando nuevas formas de deleitar a nuestros comensales.
-      </p>
-    </div>
-    <div class="image-section">
-      <FancyBoxTaquizas></FancyBoxTaquizas>
+<!-- Contenido de la página -->
+<div v-if="!isLoading">
+  <div class="banner-container">
+    <div class="banner-background"></div>
+    <div class="container">
+      <div class="text-wrapper">
+        <h1 id="bannerText">ORDER.COM</h1>
+      </div>
+      <div class="button-wrapper">
+        <h1 class="bienvenido">Bienvenido</h1>
+      </div>
     </div>
   </div>
-        <div>
-            <h1 color="#AE0505">Reseñas</h1>
-            <br>
-            <RatingTaquiza></RatingTaquiza>
-        </div>
-        <div>
-            <FancyBoxLanding></FancyBoxLanding>
-        </div>
-   
-    <Footer />
-    </div>
-  </template>
-  
-  <script>
-export default {
-  methods: {
-    goToTransporte() {
-      this.$router.push('/transporte');
-    },
-    goToComida() {
-      this.$router.push('/comida');
-    },
-    goToUtileria() {
-      this.$router.push('/utileria');
-    },
-    
 
+    <div class="container">
+      <Carousel />
+      <div class="title">
+        <h1>Servicios</h1>
+      </div>
+      <div class="row" id="cards">
+        <div class="col">
+          <div class="card">
+            <h3>Transporte</h3>
+            <div class="img-container">
+              <img
+                src="../../assets/transporteIcon.png"
+                alt=""
+                class="iconService"
+              />
+            </div>
+            <div>
+              <b-button @click="goToLogin" class="btn">Ver más</b-button>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card black-card">
+            <h3>Comida</h3>
+            <div class="img-container">
+              <img src="../../assets/comidaIcon.png" alt="" class="iconService" />
+            </div>
+            <div>
+              <b-button @click="goToLogin" class="btn">Ver más</b-button>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card">
+            <h3>Utileria</h3>
+            <div class="img-container">
+              <img
+                src="../../assets/utileriaIcon.png"
+                alt=""
+                class="iconService"
+              />
+            </div>
+            <div>
+              <b-button @click="goToLogin" class="btn">Ver más</b-button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+       <div class="servicios">
+        <div>
+          <h1 style="margin-bottom: 50px">Opiniones</h1>
+          <RatingTaquiza></RatingTaquiza>
+        </div>
+      </div>
+
+    </div> 
+
+  </div>
+
+    <Footer />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+
+  methods: {
+    goToLogin() {
+      this.$router.push("/login");
+    },
   },
 };
 </script>
 
 <style scoped>
-body,
-h2,
-p,
-ul,
-li {
+.banner-container {
+  background: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),
+    url(../../assets/BannerLandingImage.jpg);
+  width: 100%;
+  height: 100vh;
+  background-size: cover;
+  background-repeat: no-repeat;
+  position: relative;
+  margin-top: auto;
+  margin-bottom: 75px;
+}
+.banner-container .container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100%;
+}
+
+#bannerText {
+  color: white;
+  font-size: 6rem;
+}
+
+.banner-button {
+  font-family: "Montserrat", sans-serif;
+  width: 25%;
+  border-radius: 15px;
+  font-weight: bold;
+  padding: 15px;
+  background-color: white !important;
+  color: #2d2a2a;
+  transition: all 0.2s ease;
+  border: none;
+}
+
+.banner-button:hover {
+  background-color: #2d2a2a !important;
+  color: white;
+}
+
+.card {
+  background-color: #ae0505;
+  height: 100%;
+  border-radius: 16px;
+  color: white;
+  padding: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.black-card {
+  background-color: #2d2a2a;
+}
+
+.card .btn {
+  margin-top: 20px;
+  padding: 9px 50px;
+  border-radius: 10px;
+  background-color: white;
+  font-weight: 700;
+  color: #2d2a2a;
+}
+
+.card h3 {
+  margin-bottom: 20px;
+}
+
+.img-container {
+  display: flex;
+  justify-content: center;
+  height: 200px;
+}
+
+.iconService {
+  object-fit: contain;
+}
+
+.title {
+  margin-top: 50px;
+  margin-bottom: 50px;
+}
+
+#cards {
+  margin-bottom: 50px;
+}
+
+.bienvenido{
+  color: white;
+  font-size: 4rem;
+}
+
+
+@media (max-width: 800px) {
+  #app {
   margin: 0;
   padding: 0;
+  box-sizing: border-box; /* Esto asegura que el padding no afecte el ancho total de los elementos */
 }
-
-h1{
-    margin: 10vh;
-}
-
-h3{
-    margin-top: 2.5vh;
-}
-.card1{
-    background-color: #AE0505;
-    height: auto;
-    width: auto;
-    border-radius: 16px;
-    color: white;
-    font-family: 'Montserrat', sans-serif;
-    padding:2.5vh;
-    margin-bottom: 5vh;
-}
-
-.card2{
-    background-color: #2D2A2A;
-    height: auto;
-    width: auto;
-    border-radius: 16px;
-    color:white;
-    font-family: 'Montserrat', sans-serif;
-    padding: 2.5vh;
-    margin-bottom: 5vh;
-}
-
-
-img {
-  width: 100%;
-  height: 75vh;
-  margin-bottom: 5vh;
-}
-
-.title{margin-top: 1vh;}
-
-.iconService{
-    height: 15vh;
-    width: 15vh;
-}
-
-.servicios{
+  #cards {
     display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    margin: 5vh;
-    grid-gap: 5vh;
-}
-
-.btn{
-    background-color: #d9d9d9;
-    color:#2D2A2A;
-    margin: 2.5vh;
-}
-
-@media (max-width: 800px) { 
-  .servicios {
-    display: grid;
-    grid-template-columns: 1fr; /* Cambia a una sola columna */
-    margin: 2.5vh 5%; /* Ajusta el margen para pantallas pequeñas */
-    width: 90%;
+    grid-template-columns: 1fr; /* Todos los elementos se alinean en una sola columna */
+    grid-gap: 20px; /* Ajusta el espacio entre las tarjetas */
   }
 
-  h1, h2, h3, h4 {
-    text-align: center; /* Centra los títulos */
-    margin: 5vh; /* Ajusta el margen superior */
-    font-size: 1.5rem; /* Reduce el tamaño del texto para pantallas pequeñas */
+  .card {
+    justify-content: center; /* Centra el contenido en la tarjeta */
+    padding: 20px; /* Reduce el padding para ahorrar espacio */
+    text-align: center; /* Asegura que el texto esté centrado */
   }
 
-  .card1, .card2 {
-    width: 90%; /* Aumenta el ancho de las tarjetas para llenar más espacio */
-    margin-left: 5%; /* Ajusta los márgenes para centrar las tarjetas */
-    margin-right: 5%; /* Asegura que el margen derecho también esté ajustado */
-    height: auto; /* Hace que la altura se ajuste al contenido */
-  }
-
-  img {
-    height: auto; /* Ajusta la altura de la imagen para mantener la proporción */
-    width: auto; /* Asegura que la imagen no exceda el ancho de la pantalla */
-    max-width: 100%; /* Limita el ancho de la imagen al 100% del contenedor */
+  .img-container {
+    height: auto; /* Ajusta la altura del contenedor de la imagen */
+    margin-bottom: 20px; /* Añade un margen abajo de la imagen */
   }
 
   .iconService {
-    height: 10vh; /* Ajusta el tamaño de los íconos para pantallas pequeñas */
-    width: 10vh;
+    height: 15vh; /* Ajusta el tamaño de la imagen */
+    width: auto; /* Asegura que la imagen mantenga su proporción */
   }
 
-  .btn {
-    width: 50%; /* Ajusta el ancho del botón para pantallas pequeñas */
-    margin: 1vh auto; /* Centra el botón horizontalmente y ajusta el margen vertical */
+  .card .btn {
+    width: 100%; /* El botón ocupa todo el ancho disponible */
+    padding: 10px 0; /* Ajusta el padding verticalmente */
   }
+
+  #bannerText {
+  font-size: 4rem;
 }
 
-
+.bienvenido{
+  font-size: 3rem;
+}
+}
 </style>
