715e0db9b52daffd88a32c744a8478975459657c
Cisnemon13 <104937989+Cisnemon13@users.noreply.github.com>
2024-03-27 19:05:44 -0600

    Modificación de Carousel y Register Client
    -Se modificó Carousel y se agregó Spinner y más dinamismo.
    -Register fue modificado para poder acceder a la nueva vista "Vista Token"
    -Vista Token es para confirmar el token y se reedirige a Login
    -Error solo es la vista para cuando no se encuentre la pagina

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -5,7 +5,8 @@
       v-model="slide"
       :interval="4000"
       controls
-      indicators
+      :next-label="'Siguiente'" 
+      :prev-label="'Anterior'" 
       background="#ababab"
       img-width="1024"
       img-height="480"
@@ -13,64 +14,54 @@
       @sliding-start="onSlideStart"
       @sliding-end="onSlideEnd"
     >
-      <!-- Text slides with image -->
-      <b-carousel-slide
-        caption="First slide"
-        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
-        img-src="https://picsum.photos/1024/480/?image=52"
-      ></b-carousel-slide>
-
-      <!-- Slides with custom text -->
-      <b-carousel-slide img-src="../assets/Banner.png">
-        <h1>Hello world!</h1>
+    <b-carousel-slide>
+        <template #img>
+          <img
+            class="d-block img-fluid w-100"
+            src="../assets/comida2.jpg"
+            alt="Imagen 1"
+          >
+        </template>
+        <div class="carousel-caption d-none d-md-block">
+          <h5>¡Prueba nuestros buffetes!</h5>
+        </div>
       </b-carousel-slide>
 
-      <!-- Slides with image only -->
-      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
-
-      <!-- Slides with img slot -->
-      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
       <b-carousel-slide>
         <template #img>
           <img
             class="d-block img-fluid w-100"
-            width="1024"
-            height="480"
-            src="../assets/Carrousel.png"
-            alt="image slot"
+            src="../assets/comida3.jpg"
+            alt="Imagen 1"
           >
         </template>
+        <div class="carousel-caption d-none d-md-block">
+          <h5>¡Cónoce nuestros mejores paquetes!</h5>
+        </div>
       </b-carousel-slide>
-
-      <!-- Slide with blank fluid image to maintain slide aspect ratio -->
-      <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros felis, tincidunt
-          a tincidunt eget, convallis vel est. Ut pellentesque ut lacus vel interdum.
-        </p>
-      </b-carousel-slide>
-    </b-carousel>
-
+  </b-carousel>
+      
+     
   </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        slide: 0,
-        sliding: null
-      }
+export default {
+  data() {
+    return {
+      slide: 0,
+      sliding: null
+    }
+  },
+  methods: {
+    onSlideStart(slide) {
+      this.sliding = true;
     },
-    methods: {
-      onSlideStart(slide) {
-        this.sliding = true
-      },
-      onSlideEnd(slide) {
-        this.sliding = false
-      }
+    onSlideEnd(slide) {
+      this.sliding = false;
     }
   }
+}
 </script>
 
 <style>
@@ -79,5 +70,9 @@
     height: 20%;
     margin-left: 7.5%;
     margin-bottom: 0;
+}
+h5{
+  font-size: 2.5rem;
+  font-family: 'Montserrat', sans-serif;
 }
 </style>
