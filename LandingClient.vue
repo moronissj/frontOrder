06b30be62070246ee94ed8ca1c5690670708d994
Cisnemon13 <104937989+Cisnemon13@users.noreply.github.com>
2024-03-27 18:55:56 -0600

    Cambios con respecto al Carousel y Landing Client
    En Carousel se agregaron fotos más especificas y se les ajustó el tamaño que llevará el Carrusel.
    En Landing Client se agregó spinner para toda la vista.
    Se agregó un componente nuevo llamado "Vista token" para verificación del token.
    Se modificó "Register Client" para mandar al componente "Vista token"

---
+++ LandingClient.vue
@@ -0,0 +1,287 @@
+<script setup>
+import NavbarClient from './NavbarClient.vue';
+import Footer from '../../components/Footer.vue';
+import Carousel from '../../components/Carousel.vue';
+import RatingTaquiza from '../RatingTaquiza.vue';
+import FancyBoxLanding from '../FancyBoxLanding.vue';
+import FancyBoxTaquizas from '../FancyBoxTaquizas.vue';
+import { ref, onMounted } from 'vue';
+
+const isLoading = ref(true); // Estado para controlar la visualización del spinner
+
+onMounted(() => {
+  setTimeout(() => {
+    isLoading.value = false; // Oculta el spinner después de 1 segundo (ajusta según necesidad)
+  }, 5000);
+});
+
+</script>
+
+<template>
+   <div>
+
+    <div v-if="isLoading" class="spinner-fullscreen">
+      <div class="spinner-border text-primary" role="status" style="width: 20rem; height: 20rem;">
+      </div>
+    </div>
+
+  
+
+    <div>
+      <div v-if="!isLoading">
+      <div class="app">
+        <NavbarClient/>
+      </div>
+    </div>
+
+    <div class="banner-container">
+      <img src="../../assets/Banner.png" alt="" class="banner-image">
+
+    </div>
+
+
+    <Carousel/>
+    
+    <div class="title">
+        <h1>Servicios populares</h1>
+    </div>
+    <div class="servicios">     
+        <div class="card1" >
+            <h3>Transporte</h3>
+            <img src="../../assets/transporteIcon.png" alt="" class="iconService">
+            <div>
+              <b-button variant="primary" style="width: 30%;" @click="goToTransporte">Ver Más</b-button>
+            </div>
+        </div>
+        <div class="card2">
+            <h3>Comida</h3>
+            <img src="../../assets/comidaIcon.png" alt="" class="iconService">
+            <div>
+              <b-button variant="primary" style="width: 30%;"@click="goToComida">Ver Más</b-button>
+            </div>
+        </div>
+        <div class="card1">
+            <h3>Utileria</h3>
+            <img src="../../assets/utileriaIcon.png" alt="" class="iconService">
+            <div>
+              <b-button variant="primary" style="width: 30%;" @click="goToUtileria">Ver Más</b-button>
+            </div>
+            
+        </div>
+
+    </div>
+
+    <div class="quienes-somos-container">
+    <div class="header-section">
+      <h1>¿Quiénes somos?</h1>
+    </div>
+    <div class="content-section">
+      <p>
+        Somos un equipo apasionado por la gastronomía y el buen servicio, dedicado a
+        ofrecer experiencias culinarias únicas a través de nuestros buffets. Con una
+        amplia variedad de opciones que se adaptan a cualquier tipo de evento, nuestro
+        compromiso es garantizar la satisfacción y superar las expectativas de nuestros
+        clientes.
+      </p>
+      <p>
+        Fundada en [Año], nuestra misión ha sido siempre llevar a cada evento un toque
+        especial, combinando sabores tradicionales y contemporáneos para crear menús
+        memorables. Valoramos la calidad, la innovación y la excelencia en el servicio,
+        y estamos constantemente explorando nuevas formas de deleitar a nuestros comensales.
+      </p>
+    </div>
+    <div class="image-section">
+      <FancyBoxTaquizas></FancyBoxTaquizas>
+    </div>
+  </div>
+        <div>
+            <h1 color="#AE0505">Reseñas</h1>
+            <br>
+            <RatingTaquiza></RatingTaquiza>
+        </div>
+        <div>
+            <FancyBoxLanding></FancyBoxLanding>
+        </div>
+   
+    <Footer />
+    </div>
+   </div>
+</template>
+  
+  <script>
+export default {
+  methods: {
+    goToTransporte() {
+      this.$router.push('/transporte');
+    },
+    goToComida() {
+      this.$router.push('/comida');
+    },
+    goToUtileria() {
+      this.$router.push('/utileria');
+    },
+
+  },
+};
+</script>
+
+<style scoped>
+body,
+h2,
+p,
+ul,
+li {
+  margin: 0;
+  padding: 0;
+}
+
+h1{
+    margin: 10vh;
+}
+
+h3{
+    margin-top: 2.5vh;
+}
+.card1{
+    background-color: #AE0505;
+    height: auto;
+    width: auto;
+    border-radius: 16px;
+    color: white;
+    font-family: 'Montserrat', sans-serif;
+    padding:2.5vh;
+    margin-bottom: 5vh;
+}
+
+.card2{
+    background-color: #2D2A2A;
+    height: auto;
+    width: auto;
+    border-radius: 16px;
+    color:white;
+    font-family: 'Montserrat', sans-serif;
+    padding: 2.5vh;
+    margin-bottom: 5vh;
+}
+
+
+img {
+  width: 100%;
+  height: 75vh;
+  margin-bottom: 5vh;
+}
+
+
+
+.title {
+    margin-top: 5vh;
+    justify-content: center;
+    background-color: #AE0505;
+    color: #fff;
+    padding: 20px;
+    height: 25vh;
+}
+
+.iconService{
+    height: 15vh;
+    width: 15vh;
+}
+
+.servicios{
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    margin: 5vh;
+    grid-gap: 5vh;
+}
+
+
+
+@media (max-width: 800px) { 
+  .servicios {
+    display: grid;
+    grid-template-columns: 1fr; /* Cambia a una sola columna */
+    margin: 2.5vh 5%; /* Ajusta el margen para pantallas pequeñas */
+    width: 90%;
+  }
+
+  h1, h2, h3, h4 {
+    text-align: center; /* Centra los títulos */
+    margin: 5vh; /* Ajusta el margen superior */
+    font-size: 1.5rem; /* Reduce el tamaño del texto para pantallas pequeñas */
+  }
+
+  .card1, .card2 {
+    width: 90%; /* Aumenta el ancho de las tarjetas para llenar más espacio */
+    margin-left: 5%; /* Ajusta los márgenes para centrar las tarjetas */
+    margin-right: 5%; /* Asegura que el margen derecho también esté ajustado */
+    height: auto; /* Hace que la altura se ajuste al contenido */
+  }
+
+  img {
+    height: auto; /* Ajusta la altura de la imagen para mantener la proporción */
+    width: auto; /* Asegura que la imagen no exceda el ancho de la pantalla */
+    max-width: 100%; /* Limita el ancho de la imagen al 100% del contenedor */
+  }
+
+  .iconService {
+    height: 10vh; /* Ajusta el tamaño de los íconos para pantallas pequeñas */
+    width: 10vh;
+  }
+
+  .btn {
+    width: 50%; /* Ajusta el ancho del botón para pantallas pequeñas */
+    margin: 1vh auto; /* Centra el botón horizontalmente y ajusta el margen vertical */
+  }
+}
+
+.quienes-somos-container {
+
+padding: 50px 20px;
+text-align: center;
+background-color: #AE0505;
+}
+
+.header-section h1 {
+font-size: 2.5rem;
+font-family: 'Montserrat', sans-serif;
+color: white;
+margin-bottom: 30px;
+}
+
+.content-section p {
+font-family: Georgia, 'Times New Roman', Times, serif;
+font-size: 1.2rem;
+line-height: 1.6;
+color: white;
+margin-bottom: 20px;
+}
+
+.spinner-fullscreen {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw; /* Asegura que el contenedor del spinner ocupe toda la vista en ancho */
+  height: 100vh; /* Asegura que el contenedor del spinner ocupe toda la vista en alto */
+  position: fixed; /* Posicionamiento fijo para que el spinner no se mueva al hacer scroll */
+  top: 0;
+  left: 0;
+  z-index: 1050; /* Asegura que el spinner esté sobre otros elementos */
+  background-color: rgba(255, 255, 255, 0.9); /* Fondo ligeramente transparente para el spinner */
+  color: #ae0505;
+}
+.spinner-border {
+  width: 100px; /* Tamaño del spinner */
+  height: 100px;
+  border-width: 30px; /* Hace que el borde del spinner sea más grueso */
+}
+
+.loading-text {
+  margin-top: 2rem; /* Espacio entre el spinner y el texto */
+  font-size: 2rem; /* Tamaño del texto, ajusta según necesidad */
+  color: #0275d8; /* Color del texto, puedes ajustarlo para que coincida con el estilo de tu sitio */
+}
+
+
+
+</style>
+
---
+++ VistaToken.vue
@@ -0,0 +1,176 @@
+<script setup>
+import NavBar from './NavBar.vue';
+</script>
+<template>
+    <div class="app">
+        <NavBar />
+
+        <div class="content">
+            <div class="image-container">
+                <img src="../assets/buffe.PNG" alt="nose">
+            </div>
+
+            <div class="forms">
+                <h2>¡Te hemos enviado un token a tu correo!</h2>
+
+                <div class="form">
+                    <label for="token">Token</label>
+                    <input id="token" type="text" placeholder="Escribe tu token" />
+                </div>
+                <b-button @click="verifyToken">Verificar Token</b-button>
+                <div>
+                    <b-alert :show="showAlert" variant="success" ref="alertRef" style="margin-top: 10px;">
+                        <h4 class="alert-heading">¡Cuenta Verificada!</h4>
+                        <p>
+                            Tu cuenta ha sido verificada, puedes iniciar sesión.
+                        </p>
+                        <hr>
+                    </b-alert>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+  import NavBar from './NavBar.vue';
+  </script>
+
+<script>
+export default {
+    data() {
+        return {
+            showAlert: false, // Controla la visibilidad de la alerta
+        };
+    },
+
+    methods: {
+        verifyToken() {
+            this.showAlert = true; // Muestra la alerta
+
+            // Espera 5 segundos antes de redirigir
+            setTimeout(() => {
+                this.showAlert = false; // Opcional: Oculta la alerta antes de redirigir
+                this.$router.push('/login'); // Cambia '/ruta-especifica' por tu ruta objetivo
+            }, 5000);
+        },
+    },
+}
+</script>
+
+<style scoped>
+body,
+h1,
+h2,
+p,
+ul,
+li {
+    margin: 0;
+    padding: 0;
+}
+
+.app {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    /* Ensures app fills the viewport height */
+    justify-content: center;
+    align-items: center;
+}
+
+.content {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    margin: 7.5vh 15%;
+    box-shadow: 10px 5px 5px rgba(109, 109, 109, 0.5);
+    /* Consistent box-shadow formatting */
+    border-radius: 20px;
+    height: 75vh;
+}
+
+h2 {
+    font-family: 'Montserrat', sans-serif;
+    margin-top: 1.5vh;
+    margin-bottom: 1.5vh;
+    text-align: center;
+    /* Center text within heading */
+}
+
+label {
+    font-family: 'Montserrat', sans-serif;
+    font-size: 1.2rem;
+    text-align: center;
+    /* Align labels to the center */
+    margin-bottom: 2vh;
+}
+
+.forms {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    /* Centrar los elementos hijos horizontalmente */
+    justify-content: center;
+    /* Centrar los elementos hijos verticalmente */
+    padding: 2vh;
+}
+
+.form {
+    margin-bottom: 2vh;
+}
+
+input {
+    width: 80%;
+    height: 5vh;
+    border-radius: 20px;
+    margin-bottom: 1.5vh;
+    border: 3px solid #AE0505;
+    /* Consistent border color formatting */
+}
+
+.btn {
+    background-color: #2D2A2A;
+    color: white
+}
+
+
+.msg {
+    color: #AE0505;
+}
+
+.message {
+    display: flex;
+    justify-content: center;
+    margin-top: 2.5vh;
+}
+
+img {
+    margin-bottom: 0;
+    height: 75vh;
+    width: auto;
+}
+
+
+@media (max-width: 800px) {
+
+    /* Ajusta 600px al punto de ruptura deseado */
+    label {
+        font-size: 0.9rem;
+    }
+
+    .btn {
+        font-size: 0.9rem;
+        background-color: #2D2A2A;
+        width: 90%;
+        margin-top: 1vh;
+        margin-bottom: 1vh;
+    }
+
+    input {
+        font-size: 0.75rem;
+    }
+
+    img {
+        width: 100%;
+    }
+}
+</style>
